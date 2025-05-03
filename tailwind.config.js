428fe786ab860317253f9b687e9d1ecb7fe6d5e0
SpreadSheets600 <sohammaity006@gmail.com>
2025-05-03 14:35:05 +0530

    feat: project setup

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+tailwind.config.js;
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./**/*.{html,js,ts,jsx,tsx,mdx}", "./src/**/*.{html,js,ts,jsx,tsx,mdx}"],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
