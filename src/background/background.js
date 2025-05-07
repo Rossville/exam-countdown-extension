7be3cc8215146668c28cd39f05054d8254f5760d
SpreadSheets600 <sohammaity006@gmail.com>
2025-05-07 22:58:23 +0530

    feat: added load wallpers to store it in browser storage

--- src/background/background.js
+++ src/background/background.js
@@ -11,14 +11,14 @@ async function fetchExamDates() {
 		const response = await fetch("https://cdn.jsdelivr.net/gh/NovatraX/exam-countdown-extension@main/assets/exam-info.json");
 
 		if (!response.ok) {
-			throw new Error(`Failed to fetch exam dates: ${response.status}`);
+			throw new Error(`Failed To Fetch Exam Dates : ${response.status}`);
 		}
 
 		const exams = await response.json();
 		const countdowns = {
-			jee: {date: new Date(2026, 0, 29).toISOString(), isActive: true},
-			neet: {date: new Date(2026, 4, 4).toISOString(), isActive: true},
-			jeeAdv: {date: new Date(2026, 4, 18).toISOString(), isActive: true},
+			jee: { date: new Date(2026, 0, 29).toISOString(), isActive: true },
+			neet: { date: new Date(2026, 4, 4).toISOString(), isActive: true },
+			jeeAdv: { date: new Date(2026, 4, 18).toISOString(), isActive: true },
 		};
 
 		exams.forEach((exam) => {
@@ -41,20 +41,42 @@ async function fetchExamDates() {
 			showJEEADV: true,
 		});
 
-		console.log("Fetched and stored exam dates from remote source.");
+		console.log("Fetched And Stored Exam Details From Remote Source");
 	} catch (error) {
-		console.error("Error fetching exam dates:", error);
+		console.error("Error Fetching Exam Dates : ", error);
+	}
+}
+
+async function fetchWallpapers() {
+	try {
+		const response = await fetch("https://cdn.jsdelivr.net/gh/NovatraX/exam-countdown-extension@refs/heads/main/assets/wallpapers.json");
+
+		if (!response.ok) {
+			throw new Error(`Failed To Fetch Wallpaper List : ${response.status}`);
+		}
+
+		const { images = [] } = await response.json();
+
+		if (Array.isArray(images) && images.length > 0) {
+			await browser.storage.sync.set({ wallpapers: images });
+		}
+
+		console.log("Featched And Stored Wallpapers From Remote Source");
+	} catch (error) {
+		console.log("Error Fetching Exam Dates : ", error);
 	}
 }
 
 browser.runtime.onInstalled.addListener(() => {
 	console.log("Extension Installed - Fetching Data");
 	fetchExamDates();
+	fetchWallpapers();
 });
 
 browser.runtime.onStartup.addListener(() => {
 	console.log("Browser Started - Fetching Data");
 	fetchExamDates();
+	fetchWallpapers();
 });
 
 browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
